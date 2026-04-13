f993ec6f68a7a0a567c66f1933e077ef39698ce5
Tiidussalu <nite9974@gmail.com>
2026-04-13 12:36:08 +0300

    push

--- resources/js/Pages/Films/Index.vue
+++ resources/js/Pages/Films/Index.vue
@@ -42,7 +42,7 @@
                     @click="activeTab = 'external'"
                     :class="['px-5 py-2 rounded-lg text-sm font-medium transition', activeTab === 'external' ? 'bg-white shadow text-gray-900' : 'text-gray-500 hover:text-gray-700']"
                 >
-                    🌐 Väliste API
+                    🌐 Väline API
                 </button>
                 <button
                     v-if="$page.props.auth.user"
